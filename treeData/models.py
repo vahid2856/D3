f39bdc976d4d5322c1b7f894fd2c8a16eaa4a966
vahidnadarkhani <vahid.nadarkhani@gmail.com>
2021-03-16 13:07:56 +0330

    displayNodesInterval

--- treeData/models.py
+++ treeData/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class Node(models.Model):
     name = models.CharField(max_length=100)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    # parent = parent_id.name
 
     def __str__(self):
         return self.name
--- treeData/views.py
+++ treeData/views.py
@@ -8,32 +8,10 @@ def index(request):
     nodes = Node.objects.all()
     node_serializer = NodeSerializer(nodes, many=True)
     data = []
+
     for node_data in node_serializer.data:
         node_dic = dict(node_data)
         data.append(node_dic)
-
-    # print("=================================================")
-    # print(type(node_serializer.data))
-    # print("=================================================")
-    # print(node_serializer.data)
-    # print("=================================================")
-    #
-
-    #
-    # data = [{"name": "Level 2: A", "parent": "Top Level"},
-    #         {"name": "Top Level", "parent": "null"},
-    #         {"name": "Son of A", "parent": "Level 2: A"},
-    #         {"name": "Daughter of A", "parent": "Level 2: A"},
-    #         {"name": "Level 2: B", "parent": "Top Level"},
-    #         {"name": "lvl4: A", "parent": "Son of A"},
-    #         {"name": "lvl4: B", "parent": "Son of A"},
-    #         {"name": "lvl4: C", "parent": "Son of A"},
-    #         {"name": "lvl3: A", "parent": "Level 2: B"},
-    #         {"name": "lvl3: B", "parent": "Level 2: B"},
-    #         {"name": "lvl3: C", "parent": "Level 2: B"},
-    #         {"name": "lvl4: A", "parent": "lvl3: B"},
-    #         {"name": "lvl4: B", "parent": "lvl3: B"},
-    #         ]
 
     context = {
         'data': json.dumps(data),
